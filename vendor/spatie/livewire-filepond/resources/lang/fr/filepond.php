dcc6bf9837d3c7277a6ac1602f40959813bd920f
pgminguole <pgminguole@gmail.com>
2026-07-06 14:13:40 +0000

    Add gitignore and remove vendor/node_modules

--- vendor/spatie/livewire-filepond/resources/lang/fr/filepond.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'labelIdle' => 'Faites glisser vos fichiers ou <span class = "filepond--label-action"> Parcourir </span>',
-];
--- vendor/spatie/livewire-filepond/resources/lang/ja/filepond.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'labelIdle' => 'ファイルをドラッグ&ドロップ又は<span class="filepond--label-action">ファイル選択</span>',
-];
